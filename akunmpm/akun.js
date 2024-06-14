6a07e80e503318cf65f31bbe27f35b7dbb4729dd
blackhole <bimaardiantopramana@gmail.com>
2024-06-14 08:46:13 +0700

    asdasd

--- akunmpm/akun.js
+++ akunmpm/akun.js
@@ -95,6 +95,7 @@ let users = [
     { name: 'Sinta dwi cahyani', email: 'sintacika01@gmail.com', password: 'sinta1', branch: 'Malang', position: 'Sales Online', cabang: 'MPM Motor Kediri' },
     { name: 'Rochmanul karim', email: 'rochmanulkarim1@gmail.com', password: 'Cakkarim07@', branch: 'KAB. PASURUAN', position: 'SALES ONLINE', cabang: 'mpm sukorejo' },
     { name: 'AJI ADITYA PRAMANA', email: 'ajiapram4@gmail.com', password: 'ajiap1234', branch: 'KAB PASURUAN', position: 'KAB PASURUAN', cabang: 'MPM SUKOREJO' },
+    { name: 'Rizky firmansyah', email: 'firmansyahrizky423@gmail.com', password: 'Rafly22', branch: 'KAB PASURUAN', position: 'SALES ONLINE', cabang: 'Sukorejo' },
     { name: '', email: '', password: '', branch: '', position: '', cabang: '' },
     // Rapikan code dengan Shift + Alt + F
 ];
