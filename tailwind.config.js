be2560ff30dc0f9815f8deb105d49918e62e1547
blackhole <bimaardiantopramana@gmail.com>
2024-06-16 19:16:25 +0700

    aaaaaaaaaaaaaaaa

---
+++ tailwind.config.js
@@ -0,0 +1,88 @@
+module.exports = {
+  "theme": {
+    "extend": {
+      "colors": {
+        "theme": {
+          "primary2": "#923A06",
+          "primary1": "#D1510A",
+          "accent1": "#F3E8E2",
+          "neutral-light": "#FBFAF9",
+          "secondary1": "#F3E8E2",
+          "accent2": "#D8BFAF",
+          "neutral-dark": "#191818",
+          "secondary2": "#E0D1CB"
+        },
+        "Primary": {
+          "100": "#003EB3",
+          "300": "#0074F0",
+          "500": "#14A9FF",
+          "700": "#85DCFF"
+        },
+        "Danger": {
+          "300": "#A22020",
+          "500": "#BF2626",
+          "700": "#E14747"
+        },
+        "grays": {
+          "white100": "#FFFFFF"
+        },
+        "Gray": {
+          "500": "#595959",
+          "700": "#999999",
+          "900": "#D9D9D9",
+          "Black": "#000000",
+          "White": "#FFFFFF"
+        },
+        "backgrounds": {
+          "gray": "#f7f4f2ff",
+          "primary": "#fa7930ff"
+        },
+        "Success": {
+          "300": "#199033",
+          "500": "#32A94C",
+          "700": "#4CC366"
+        },
+        "foreground": "#000000",
+        "background": "#FFFFFF"
+      },
+      "spacing": {
+        "FiveUnits": "80px",
+        "FourUnits": "64px",
+        "OneAndHalfUnits": "24px",
+        "TwoUnits": "32px",
+        "HalfUnit": "8px",
+        "ThreeUnits": "48px",
+        "Unit": "16px",
+        "SixUnits": "96px"
+      },
+      "borderRadius": {
+        "inputradius": "24px",
+        "imageradius": "8px",
+        "cardradius": "8px",
+        "input-radius": "24px",
+        "card-radius": "8px",
+        "buttonradius": "24px",
+        "image-radius": "8px",
+        "button-radius": "24px",
+        "Radius4": "4px",
+        "Radius2": "2px",
+        "Radius8": "8px",
+        "Round": "50%"
+      },
+      "scale": {
+        "Small": "48px",
+        "MaxWidth": "1400px",
+        "XXLarge": "288px",
+        "Medium": "96px",
+        "XLarge": "192px",
+        "XSmall": "16px",
+        "Large": "144px"
+      }
+    }
+  },
+  "plugins": [],
+  "content": [
+    "./src/**/*.{html,js,ts,jsx,tsx}",
+    "./*.html"
+  ]
+}
