23557941d09a867d7178f833df65b9325810bf27
blackhole <bimaardiantopramana@gmail.com>
2024-06-17 09:48:09 +0700

    sddddddddddddd

--- sayang/profile.js
+++ sayang/profile.js
@@ -1,57 +1,49 @@
-const profile1 = document.getElementById('profile1');
-const profile2 = document.getElementById('profile2');
-const upload = document.getElementById('upload');
-const deleteBtn = document.getElementById('delete');
-const applyBtn = document.getElementById('apply');
+<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
+<script>
+$(document).ready(function() {
+    // Handle file upload
+    $('#upload').on('change', function() {
+        if (this.files && this.files[0]) {
+            var reader = new FileReader();
+            reader.onload = function(e) {
+                $('.profile-pic').attr('src', e.target.result);
+            }
+            reader.readAsDataURL(this.files[0]);
+        }
+    });
 
-function loadProfilePictures() {
-    const savedProfile1 = localStorage.getItem('profile1');
-    const savedProfile2 = localStorage.getItem('profile2');
+    // Save images to local storage
+    $('#saveButton').on('click', function() {
+        var images = [];
+        $('.profile-pic').each(function() {
+            var src = $(this).attr('src');
+            if (src) {
+                images.push(src);
+            }
+        });
+        if (images.length > 0) {
+            localStorage.setItem('profileImages', JSON.stringify(images));
+            alert('Gambar profil telah disimpan.');
+        } else {
+            alert('Tidak ada gambar untuk disimpan.');
+        }
+    });
 
-    if (savedProfile1) {
-        profile1.src = savedProfile1;
-    }
+    // Delete images from local storage and page
+    $('#deleteButton').on('click', function() {
+        $('.profile-pic').attr('src', '');
+        localStorage.removeItem('profileImages');
+        alert('Gambar profil telah dihapus.');
+    });
 
-    if (savedProfile2) {
-        profile2.src = savedProfile2;
-    }
-}
-
-function updateProfilePictures(file) {
-    const reader = new FileReader();
-
-    reader.onload = function (e) {
-        const result = e.target.result;
-        profile1.src = result;
-        profile2.src = result;
-    }
-
-    reader.readAsDataURL(file);
-}
-
-upload.addEventListener('change', (event) => {
-    const file = event.target.files[0];
-    if (file) {
-        updateProfilePictures(file);
-    }
-});
-
-deleteBtn.addEventListener('click', () => {
-    const defaultImage = 'default.jpg';
-    profile1.src = defaultImage;
-    profile2.src = defaultImage;
-    localStorage.removeItem('profile1');
-    localStorage.removeItem('profile2');
-});
-
-applyBtn.addEventListener('click', () => {
-    try {
-        localStorage.setItem('profile1', profile1.src);
-        localStorage.setItem('profile2', profile2.src);
-        alert('Profile pictures updated successfully!');
-    } catch (e) {
-        console.error('Error saving to local storage', e);
+    // Load images from local storage on page load
+    var savedImages = JSON.parse(localStorage.getItem('profileImages'));
+    if (savedImages && savedImages.length > 0) {
+        $('.profile-pic').each(function(index) {
+            if (index < savedImages.length) {
+                $(this).attr('src', savedImages[index]);
+            }
+        });
     }
 });
-
-window.onload = loadProfilePictures;
+</script>
