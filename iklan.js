435124affeac1138ca3f52c8abd86b0937efa0fd
blackhole <bimaardiantopramana@gmail.com>
2024-06-21 07:29:11 +0700

    asd

---
+++ iklan.js
@@ -0,0 +1,16 @@
+async src = "https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js"
+
+    (adsbygoogle = window.adsbygoogle || []).push({
+        gadUnit: 'pub-8913081554574910',  // Ganti dengan ID unit iklan Anda
+        format: 'auto',  // Ukuran iklan responsif
+        autoRefresh: { seconds: 30 },  // Refresh iklan setiap 60 detik (opsional)
+    });
+
+
+<ins class="adsbygoogle"
+    style="display:inline-block;width:100%;height:auto;"
+    data-ad-client="ca-app-pub-8913081554574910/6786583120"
+    data-ad-slot="ca-app-pub-8913081554574910~3291215428"></ins>
+
+(adsbygoogle = window.adsbygoogle || []).push({});
+
---
+++ sayang/pesan.js
@@ -0,0 +1,3 @@
+// pesan
+document.getElementById('note1').innerText = 'astaga';
+document.getElementById('note2').innerText = 'Teks Baru untuk Catatan 2';
