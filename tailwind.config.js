9766c98697ec87f9cf6ed3363b954883b84f09b5
blackhole <bimaardiantopramana@gmail.com>
2024-06-22 08:37:42 +0700

    asdasdasd

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,15 +2,8 @@ module.exports = {
   "theme": {
     "extend": {
       "colors": {
-        "theme": {
-          "primary2": "#923A06",
-          "primary1": "#D1510A",
-          "accent1": "#F3E8E2",
-          "neutral-light": "#FBFAF9",
-          "secondary1": "#F3E8E2",
-          "accent2": "#D8BFAF",
-          "neutral-dark": "#191818",
-          "secondary2": "#E0D1CB"
+        "Template": {
+          "blue-bg": "#5228f5ff"
         },
         "Primary": {
           "100": "#003EB3",
@@ -23,8 +16,10 @@ module.exports = {
           "500": "#BF2626",
           "700": "#E14747"
         },
-        "grays": {
-          "white100": "#FFFFFF"
+        "Success": {
+          "300": "#199033",
+          "500": "#32A94C",
+          "700": "#4CC366"
         },
         "Gray": {
           "500": "#595959",
@@ -33,29 +28,42 @@ module.exports = {
           "Black": "#000000",
           "White": "#FFFFFF"
         },
+        "foreground": "#000000",
+        "background": "#FFFFFF",
+        "theme": {
+          "primary2": "#923A06",
+          "primary1": "#D1510A",
+          "accent1": "#F3E8E2",
+          "neutral-light": "#FBFAF9",
+          "secondary1": "#F3E8E2",
+          "accent2": "#D8BFAF",
+          "neutral-dark": "#191818",
+          "secondary2": "#E0D1CB"
+        },
+        "grays": {
+          "white100": "#FFFFFF"
+        },
         "backgrounds": {
           "gray": "#f7f4f2ff",
           "primary": "#fa7930ff"
-        },
-        "Success": {
-          "300": "#199033",
-          "500": "#32A94C",
-          "700": "#4CC366"
-        },
-        "foreground": "#000000",
-        "background": "#FFFFFF"
+        }
       },
       "spacing": {
-        "FiveUnits": "80px",
-        "FourUnits": "64px",
-        "OneAndHalfUnits": "24px",
-        "TwoUnits": "32px",
-        "HalfUnit": "8px",
         "ThreeUnits": "48px",
+        "OneAndHalfUnits": "24px",
         "Unit": "16px",
-        "SixUnits": "96px"
+        "HalfUnit": "8px",
+        "FourUnits": "64px",
+        "SevenUnits": "112px",
+        "TwoUnits": "32px",
+        "SixUnits": "96px",
+        "FiveUnits": "80px"
       },
       "borderRadius": {
+        "Radius2": "2px",
+        "Round": "50%",
+        "Radius4": "4px",
+        "Radius8": "8px",
         "inputradius": "24px",
         "imageradius": "8px",
         "cardradius": "8px",
@@ -63,20 +71,16 @@ module.exports = {
         "card-radius": "8px",
         "buttonradius": "24px",
         "image-radius": "8px",
-        "button-radius": "24px",
-        "Radius4": "4px",
-        "Radius2": "2px",
-        "Radius8": "8px",
-        "Round": "50%"
+        "button-radius": "24px"
       },
       "scale": {
+        "XXLarge": "288px",
+        "XLarge": "192px",
         "Small": "48px",
         "MaxWidth": "1400px",
-        "XXLarge": "288px",
+        "Large": "144px",
         "Medium": "96px",
-        "XLarge": "192px",
-        "XSmall": "16px",
-        "Large": "144px"
+        "XSmall": "16px"
       }
     }
   },
